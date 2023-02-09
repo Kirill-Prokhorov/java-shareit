a3cd561a777c7f2b3cd139843654ba47734c053a
Kirill Prokhorov <full-la@yandex.ru>
2023-02-09 16:46:54 +0300

    Борьба с тестами на гите

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -2,8 +2,6 @@ package ru.practicum.shareit.item;
 
 import lombok.extern.slf4j.Slf4j;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemDtoWithBooking;
